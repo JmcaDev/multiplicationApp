8d80cf607364133a851a5b4a7ec9099baac406ac
JmcaDev <jmcavzla@gmail.com>
2025-05-30 19:51:46 -0400

    Adding yarg to the code's logic

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,24 +1,28 @@
 import fs from "node:fs"
+import { yarg } from "../src/config/plugins/args.plugin"
+
+const { b:base, l:limit, s:showTable} = yarg
 
 let outputMessage = ""
-const numeroTabla =10
 const headerMessage = `
 ===========================
-    Tabla del ${numeroTabla}
+    Tabla del ${base}
 ===========================\n
 `
 
-for (let index = 1; index <= 10; index++) {
-    outputMessage += `${numeroTabla} X ${index} = ${numeroTabla * index}\n`
+for (let index = 1; index <= limit; index++) {
+    outputMessage += `${base} X ${index} = ${base * index}\n`
 }
 outputMessage = headerMessage + outputMessage
-console.log(outputMessage)
+
+if( showTable ) console.log(outputMessage)
 
 const outputPath = `outputs/`
 
 fs.mkdirSync(outputPath, {recursive: true})
-fs.writeFile(`${outputPath}/tabla-${numeroTabla}.txt`, outputMessage, (error) => {
+fs.writeFile(`${outputPath}/tabla-${base}.txt`, outputMessage, (error) => {
     if(error){
         console.log(error)
     }
 })
+console.log("File created")

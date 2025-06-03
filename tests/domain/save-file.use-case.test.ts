ca11f7a69cfb18fbc6d2785e6adaca2cc8dc3319
JmcaDev <jmcavzla@gmail.com>
2025-06-02 20:40:07 -0400

    Test save-file.use-case and arg.plugins

--- tests/domain/save-file.use-case.test.ts
+++ tests/domain/save-file.use-case.test.ts
@@ -3,9 +3,10 @@ import {SaveFile} from "../../src/domain/use-cases/save-file.use-case"
 
 describe("save-file.use-case", () => {
 
-    afterEach(() => {
+    afterAll(() => {
         //clea up
         fs.rmSync("outputs", {recursive:true})
+        fs.rmSync("custom-outputs", {recursive:true})
     })
 
     test("Should save file with default values", () => {
@@ -22,5 +23,54 @@ describe("save-file.use-case", () => {
         expect(result).toBe(true)
         expect(checkFile).toBe(true)
         expect(fileContent).toBe(options.fileContent)
+    })
+
+    test("Should save file with custom values", () => {
+        const options ={
+            fileContent: "custom content",
+            fileDestination: "custom-outputs/file-destination",
+            fileName: "custom-table-name"
+        }
+        const saveFile = new SaveFile()
+
+        const path = `${options.fileDestination}/${options.fileName}.txt`
+
+        const result = saveFile.execute(options)
+        const checkFile = fs.existsSync(path)
+        const fileContent = fs.readFileSync(path, {encoding: "utf-8"})
+
+        expect(result).toBe(true)
+        expect(checkFile).toBe(true)
+        expect(fileContent).toBe(options.fileContent)
+    })
+
+    test("Should return false if directory could not be created", () => {
+
+        const options = {
+            fileContent: "test content"
+        }
+
+        const saveFile = new SaveFile()
+        const mkdirMock = jest.spyOn(fs, "mkdirSync").mockImplementation( () => {throw new Error('This is a custom error message from testing from test "Should return false if directory could not be created"')})
+
+        const result = saveFile.execute(options)
+        expect(result).toBe(false)
+
+        mkdirMock.mockRestore()
+    })
+
+    test("Should return false if file could not be created", () => {
+
+        const options = {
+            fileContent: "test content"
+        }
+
+        const saveFile = new SaveFile()
+        const mkdirMock = jest.spyOn(fs, "writeFileSync").mockImplementation( () => {throw new Error('This is a custom error message from testing from test "Should return false if file could not be created"')})
+
+        const result = saveFile.execute(options)
+        expect(result).toBe(false)
+
+        mkdirMock.mockRestore()
     })
 })
